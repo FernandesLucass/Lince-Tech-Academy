baadf8818a78e2e4bcd5847098b98947ad8159b2
Lucas Fernandes <lucas.lfernandes@outlook.com>
2026-05-18 21:36:44 -0300

    Adiciona exemplos de variáveis e listas em Dart

---
+++ Formacao Dart/01_index.dart
@@ -0,0 +1,8 @@
+void main() {
+  // var nomeCompleto = 'Lucas Fernandes';
+  // Object nomeCompleto = 'Lucas Fernandes';
+  // String nomeCompleto = 'Lucas Fernandes';
+  dynamic nomeCompleto = 'Lucas Fernandes';
+
+  print(nomeCompleto);
+}
---
+++ Formacao Dart/04_final_const.dart
@@ -0,0 +1,24 @@
+
+// void main() {
+//   final valor = getValor();
+//   const valor = getValor();  
+
+//   print(valor);
+// }
+// String getValor() => 'Valor retornado pela função';
+
+// -----------------------------------------------------------
+
+// void main() {
+//   final listaDeLinguagens = ['Dart', 'Java'];
+//   listaDeLinguagens.add('Python');
+
+//   print(listaDeLinguagens);
+// }
+
+void main() {
+  const listaDeLinguagens = ['Dart', 'Java'];
+  listaDeLinguagens.add('Python');
+
+  print(listaDeLinguagens);
+}
